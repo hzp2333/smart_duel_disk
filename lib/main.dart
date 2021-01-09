9f40bed973ba7a747c90ec75a5403c208d05664b
Bram De Coninck <bram.deconinck@inthepocket.com>
2021-01-09 17:10:54 +0100

    Add a way to keep secrets out the source code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:smart_duel_disk/packages/core/core_config/core_config_interface/lib/core_config_interface.dart';
 import 'package:smart_duel_disk/start.dart';
 
+const _secretsFilePath = './secrets.env';
+
 Future<void> main() async {
-  await start(AppConfig.release());
+  final dotEnv = DotEnv();
+  await dotEnv.load(_secretsFilePath);
+
+  await start(AppConfig.release(dotEnv));
 }
---
+++ lib/packages/core/core_config/core_config_interface/lib/src/models/app_config.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+@immutable
+class AppConfig {
+  String get twitterUrl => 'https://twitter.com/CrownCorpBE';
+  String get tweetUrl => 'https://twitter.com/{0}/status/{1}';
+  String get youtubeUrl => 'https://www.youtube.com/channel/UCz2Que3btHnGkwrW-emZV5A';
+  String get discordUrl => 'https://discord.gg/SxmeN9Yh8a';
+
+  final String twitterUserId;
+  final String twitterConsumerKey;
+  final String twitterConsumerSecret;
+  final String twitterToken;
+  final String twitterSecret;
+
+  const AppConfig._({
+    @required this.twitterUserId,
+    @required this.twitterConsumerKey,
+    @required this.twitterConsumerSecret,
+    @required this.twitterToken,
+    @required this.twitterSecret,
+  });
+
+  factory AppConfig.release(DotEnv dotEnv) {
+    return AppConfig._(
+      twitterUserId: 'CrownCorpBE',
+      twitterConsumerKey: dotEnv.env['TWITTER_CONSUMER_KEY'],
+      twitterConsumerSecret: dotEnv.env['TWITTER_CONSUMER_SECRET'],
+      twitterToken: dotEnv.env['TWITTER_TOKEN'],
+      twitterSecret: dotEnv.env['TWITTER_SECRET'],
+    );
+  }
+}
